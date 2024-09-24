bb77635175da7fcfc4bf2669f969c724f8fa6c20
chimbor victor <chimborvictor10@gmail.com>
2024-09-24 10:42:45 +0100

    changed the background image properties to allow it fit on every phone view

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,9 @@ export default function RootLayout({ children }) {
   return (
     <>
       <html lang="en" className="!scroll-smooth">
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </head>
         <body className={inter.className}>
           <CursorProvider>
             <Transition />
